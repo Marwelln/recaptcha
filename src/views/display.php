927591bf170b1b68e9bbfe670e17c3756b36917b
Martin Mårtensson <martin@redward.se>
2014-12-05 19:58:06 +0100

    The script will now only load once even if you call the view file several times

--- src/views/display.php
+++ src/views/display.php
@@ -1,3 +1,3 @@
 <div class="g-recaptcha" data-sitekey="<?= Config::get('recaptcha::siteKey'); ?>"></div>
 
-<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+<?= Marwelln\Recaptcha\Script::instance()->script(); ?>
