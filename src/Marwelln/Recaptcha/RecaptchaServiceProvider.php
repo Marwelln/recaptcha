0ae1b008274e7aa7b4d98ad720ca5cd17db03ccd
Martin Mårtensson <martin@redward.se>
2014-12-05 10:25:14 +0100

    Initial commit

---
+++ src/Marwelln/Recaptcha/RecaptchaServiceProvider.php
@@ -0,0 +1,48 @@
+<?php namespace Marwelln\Recaptcha;
+
+use Illuminate\Support\ServiceProvider;
+use Marwelln\Recaptcha\Laravel\Validator;
+
+class RecaptchaServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('marwelln/recaptcha');
+
+		$this->app->validator->resolver(function($translator, $data, $rules, $messages) {
+			return new Validator($translator, $data, $rules, $messages);
+    });
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
---
+++ src/views/display.php
@@ -0,0 +1,3 @@
+<div class="g-recaptcha" data-sitekey="<?= Config::get('recaptcha::siteKey'); ?>"></div>
+
+<script src="https://www.google.com/recaptcha/api.js" async defer></script>
